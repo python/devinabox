f698738eda857c1428701ee8979c2f1bfe32026d
Brett Cannon <brett@python.org>
2011-02-26 13:04:19 -0800

    Modularize build_cpython.py.

--- build_cpython.py
+++ build_cpython.py
@@ -5,19 +5,24 @@ import os
 import subprocess
 import sys
 
-if sys.platform == 'win32':
-    print("See the devguide's Getting Set Up guide for building under Windows")
+def main():
+    if sys.platform == 'win32':
+        print("See the devguide's Getting Set Up guide for building under "
+              "Windows")
 
-directory = 'cpython'
-cwd = os.getcwd()
-os.chdir(directory)
-try:
-    if os.path.isfile('Makefile'):
-        print('Makefile already exists; skipping ./configure')
-    else:
-        subprocess.check_call(['./configure', '--prefix=/dev/null',
-                               '--with-pydebug'])
-    make_cmd = ['make', '-s', '-j', str(multiprocessing.cpu_count())]
-    subprocess.call(make_cmd)
-finally:
-    os.chdir(cwd)
+    directory = 'cpython'
+    cwd = os.getcwd()
+    os.chdir(directory)
+    try:
+        if os.path.isfile('Makefile'):
+            print('Makefile already exists; skipping ./configure')
+        else:
+            subprocess.check_call(['./configure', '--prefix=/dev/null',
+                                   '--with-pydebug'])
+        make_cmd = ['make', '-s', '-j', str(multiprocessing.cpu_count())]
+        subprocess.call(make_cmd)
+    finally:
+        os.chdir(cwd)
+
+if __name__ == '__main__':
+    main()
