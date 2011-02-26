8842b8df683041d0589af4aeafdabfa9a07422de
Brett Cannon <brett@python.org>
2011-02-26 13:04:41 -0800

    Refactor run_tests.py.

--- run_tests.py
+++ run_tests.py
@@ -6,21 +6,27 @@ import subprocess
 import sys
 
 
-directory = 'cpython'
-cmd = os.path.join(directory, 'python')
-# UNIX
-if not os.path.isfile(cmd):
-    # OS X
-    cmd += '.exe'
+def executable():
+    directory = 'cpython'
+    cmd = os.path.join(directory, 'python')
+    # UNIX
     if not os.path.isfile(cmd):
-        # 32-bit Windows
-        cmd = os.path.join(directory, 'PCBuild', 'python_d.exe')
+        # OS X
+        cmd += '.exe'
         if not os.path.isfile(cmd):
-            # 64-bit Windows
-            cmd = os.path.join(directory, 'PCBuild', 'AMD64', 'python_d.exe')
+            # 32-bit Windows
+            cmd = os.path.join(directory, 'PCBuild', 'python_d.exe')
             if not os.path.isfile(cmd):
-                print('CPython is not built')
-                sys.exit(1)
+                # 64-bit Windows
+                cmd = os.path.join(directory, 'PCBuild', 'AMD64', 'python_d.exe')
+                if not os.path.isfile(cmd):
+                    return None
 
-subprocess.call([cmd, '-W', 'default', '-bb', '-E', '-m', 'test', '-r', '-w',
-                 '-u', 'all', '-j', str(multiprocessing.cpu_count())])
+if __name__ == '__main__':
+    cmd = executable()
+    if cmd is None:
+        print('CPython is not built')
+        sys.exit(1)
+    subprocess.call([cmd, '-W', 'default', '-bb', '-E', '-m', 'test', '-r',
+                     '-w', '-u', 'all', '-j',
+                     str(multiprocessing.cpu_count())])
